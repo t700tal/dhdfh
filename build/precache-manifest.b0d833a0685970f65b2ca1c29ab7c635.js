7eabb13a6a55612dd4a1a294b5384ca384df545b
User <user@Users-MacBook-Pro.local>
2023-05-03 01:49:27 +0300

    first commit

---
+++ build/precache-manifest.b0d833a0685970f65b2ca1c29ab7c635.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5739fbbc7da9ab0149114e0eb5c9388",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c4680b932e702503ff0",
+    "url": "/static/css/main.bb1cd6a4.chunk.css"
+  },
+  {
+    "revision": "dccad0c37137fac6069e",
+    "url": "/static/js/2.a8e6d086.chunk.js"
+  },
+  {
+    "revision": "c21d579849576bf6a47550572ff70f62",
+    "url": "/static/js/2.a8e6d086.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c4680b932e702503ff0",
+    "url": "/static/js/main.12c0bb98.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.12c0bb98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b6b46ef1e17d32e6df4",
+    "url": "/static/js/runtime-main.bce919bb.js"
+  }
+]);
